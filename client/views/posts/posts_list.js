4a8cec0336b1a0d4344ca64a0ef1e3129a9ff90a
Ryan Yogan <ryanyogan@gmail.com>
2013-05-11 17:49:52 -0500

    initial app commit

---
+++ client/views/posts/posts_list.js
@@ -0,0 +1,23 @@
+// var postsData = [
+// {
+//   title: 'Introducing Telescope',
+//   author: 'Sacha Greif',
+//   url: 'http://sachagreif.com/introducing-telescope/'
+// },
+// {
+//   title: 'Meteor',
+//   author: 'Tom Coleman',
+//   url: 'http://meteor.com'
+// },
+// {
+//   title: 'The Meteor Book',
+//   author: 'Tom Coleman',
+//   url: 'http://themeterokbook.com'
+// }
+// ];
+
+Template.postsList.helpers({
+  posts: function() {
+    return Posts.find({}, {sort: {submitted: -1}});
+  }
+});
