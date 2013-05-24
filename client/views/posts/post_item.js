7ecd89da0c2c976ad889a9d47715cfdb000f4712
Ryan Yogan <ryanyogan@gmail.com>
2013-05-23 19:20:41 -0500

    add animation for up vote

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -25,3 +25,26 @@ Template.postItem.events({
     Meteor.call('upvote', this._id);
   }
 });
+
+Template.postItem.rendered = function() {
+  // animate post from previous position to new position
+  var instance = this;
+  var rank = instance.data._rank;
+  var $this = $(this.firstNode);
+  var postHeight = 80;
+  var newPosition = rank * postHeight;
+
+  // if element has currentPosition
+  if (typeof(instance.currentPosition) !== 'undefined') {
+    var previousPosition = instance.currentPosition;
+    var delta = previousPosition - newPosition;
+    $this.css("top", delta + "px");
+  } else {
+    $this.addClass("invisible");
+  }
+
+  Meteor.defer(function() {
+    instance.currentPosition = newPosition;
+    $this.css("top", "0px").removeClass("invisible");
+  });
+};
